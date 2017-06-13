cf8295cc5a9952f5f5b0165b7fa27f29da3ea852
Matthew Menna <mattmenna@gmail.com>
2017-06-13 14:30:05 -0400

    Initial Commit

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -7,21 +7,34 @@ import java.util.ArrayList;
  */
 public class Player {
 
+    public int[] playerHand;
+    public String playerName
+
     public Player(String name) {
         System.out.println(name);
+        playerName = name;
+
     }
 
+    public void status(String name, int[] hand){
+        System.out.println(name);
+        for(int i=0; i<10; i++ ) {
+            hand[i]=i+2;
+            System.out.println(hand[i]);
+        }
+        }
+
+
     public int[] deal(){
-       int[] hand;
-       hand = new int[10];
+       int[10] hand;
 
        for(int i=0; i<10; i++ ) {
            hand[i]=i+2;
            //System.out.println(hand[i]);
        }
 
-       int[] dealtHand;
-       dealtHand = new int[2]
-       return dealthand;
+       int randCard = ;
+
+      System.out.print(hand[randCard]);
     }
 }
